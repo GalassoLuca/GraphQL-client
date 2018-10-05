aa40465365c33480c24e2afdd5bf12269971f4c6
Luca Galasso <luca@WonderLuca.local>
2018-10-05 14:12:39 +0200

    feat(get-course): promise then vs await + class with static method

--- api/get-course.js
+++ api/get-course.js
@@ -4,19 +4,23 @@ const chalk = require('chalk')
 const endpoint = 'http://localhost:4000/graphql'
 const client = new GraphQLClient(endpoint)
 
-const query = `
-query getCourse($courseID: Int!) {
-  course(id: $courseID) {
-      title
-      author
-      description
-      topic
-      url
+class Course {
+  static getByID(courseID) {
+    console.log(chalk.bold(`\nCalling getCoursePromise query for course with id ${courseID}`))
+
+    const query = `
+      query getCourse($courseID: Int!) {
+        course(id: $courseID) {
+          title
+          author
+          description
+          topic
+          url
+        }
+      }`
+
+    return client.request(query, { courseID })
   }
 }
-`
 
-module.exports = function getCoursePromise(courseID) {
-  console.log(chalk.bold(`\nCalling getCoursePromise query for course with id ${courseID}`))
-  return client.request(query, { courseID })
-}
+module.exports = Course
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const getCoursePromise = require('./api/get-course')
+const {getByID} = require('./api/get-course')
 const debug = require('debug')('get-course:get')
 const chalk = require('chalk')
 
-async function getCourse(productID) {
-  let response = await getCoursePromise(productID)
+async function getCourse(courseID) {
+  let response = await getByID(courseID)
 
-  debug(`Response for course with ID ${productID}`)
+  debug(`Response for course with ID ${courseID}`)
 
   if (response.error)
     throw new Error(response.error)
 
-  console.log(chalk.bold.green(`OK getCourse with ID ${productID}`))
+  console.log(chalk.bold.green(`OK getCourse with ID ${courseID}`))
   console.log(JSON.stringify(response, null, 2))
 
   return response
 }
 
-const response = getCourse(2)
-console.log(JSON.stringify(response, null, 2))
+(async () => {
+  const response = await getCourse(2)
+  console.log(JSON.stringify(response, null, 2))
+})()
+
+getCourse(2).then(response => {
+  console.log(JSON.stringify(response, null, 2))
+})
+
