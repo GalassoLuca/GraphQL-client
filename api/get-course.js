ff1e76aafcc316ebd5ad4a2d205d113d92f9813e
Luca Galasso <luca.galasso@wonderflow.co>
2019-09-11 10:49:27 +0200

    refactor: improve code style and readability

--- api/get-course.js
+++ api/get-course.js
@@ -1,26 +1,23 @@
 const { GraphQLClient } = require('graphql-request')
-const chalk = require('chalk')
 
 const endpoint = 'http://localhost:4000/graphql'
 const client = new GraphQLClient(endpoint)
 
-class Course {
-  static getByID(courseID) {
-    console.log(chalk.bold(`\nCalling getCoursePromise query for course with id ${courseID}`))
+function getByID (courseID) {
+  const query = `
+    query getCourse($courseID: Int!) {
+      course(id: $courseID) {
+        title
+        author
+        description
+        topic
+        url
+      }
+    }`
 
-    const query = `
-      query getCourse($courseID: Int!) {
-        course(id: $courseID) {
-          title
-          author
-          description
-          topic
-          url
-        }
-      }`
+  const variables = { courseID }
 
-    return client.request(query, { courseID })
-  }
+  return client.request(query, variables)
 }
 
-module.exports = Course
+module.exports = { getByID }
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const {getByID} = require('./api/get-course')
-const debug = require('debug')('get-course:get')
-const chalk = require('chalk')
+const { getByID } = require('./api/get-course')
+const debug = require('debug')('index')
 
-async function getCourse(courseID) {
-  let response = await getByID(courseID)
+async function getCourse (courseID) {
+  const response = await getByID(courseID)
 
-  debug(`Response for course with ID ${courseID}`)
+  debug(`getCourse with ID ${courseID}`)
 
-  if (response.error)
+  if (response.error) {
     throw new Error(response.error)
+  }
 
-  console.log(chalk.bold.green(`OK getCourse with ID ${courseID}`))
   console.log(JSON.stringify(response, null, 2))
 
   return response
@@ -24,4 +23,3 @@ async function getCourse(courseID) {
 getCourse(2).then(response => {
   console.log(JSON.stringify(response, null, 2))
 })
-
