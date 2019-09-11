a776bff7875b0b3ac3b6afab2278f4c90c759dda
Luca Galasso <luca.galasso@wonderflow.co>
2019-09-11 10:55:17 +0200

    refactor: use chalk instead of debug

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { getByID } = require('./api/get-course')
-const debug = require('debug')('index')
+const chalk = require('chalk')
 
 async function getCourse (courseID) {
   const response = await getByID(courseID)
 
-  debug(`getCourse with ID ${courseID}`)
+  console.log(chalk.bold(`getCourse with ID ${courseID}`))
 
   if (response.error) {
     throw new Error(response.error)
@@ -15,11 +15,4 @@ async function getCourse (courseID) {
   return response
 }
 
-(async () => {
-  const response = await getCourse(2)
-  console.log(JSON.stringify(response, null, 2))
-})()
-
-getCourse(2).then(response => {
-  console.log(JSON.stringify(response, null, 2))
-})
+getCourse(2)
